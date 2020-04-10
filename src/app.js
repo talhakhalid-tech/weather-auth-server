cc26d5d761374ad73396a771003cbdcb3c1f361c
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-04-10 19:02:44 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -4,13 +4,20 @@ require('../db/mongoose.js')
 const userRouter = require('../routers/users')
 const weatherRouter = require('../routers/weather')
 const bodyParser = require('body-parser');
-const cors =require('cors')
 
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin , X-Requested-With , Content-Type,Accept,x-auth-token"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET , POST , PATCH , DELETE");
+    next();
+});
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
